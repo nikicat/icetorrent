474c283c60f5046093b293fe594a650b873d823b
Nikolay Bryskin <nbryskin@yandex-team.ru>
2012-10-06 00:13:34 +0400

    dashboard replaced with transmission web ui

--- data/message.js
+++ data/message.js
@@ -1,2 +1,25 @@
-window.wrappedJSObject.postMessage = self.postMessage;
-window.wrappedJSObject.on = self.on;
+var callbacks = [];
+var tag = 0;
+
+console.info('attached');
+
+function rpc(method, arguments, callback) {
+    var mytag = tag++;
+    callbacks[mytag] = callback;
+    self.port.emit('message', {
+        method: method,
+        arguments: arguments,
+        tag: mytag
+    });
+}
+
+window.wrappedJSObject.rpc = rpc;
+
+self.port.on('message', function(message) {
+    //console.info('message: '+JSON.stringify(message));
+    var callback = callbacks[message.tag];
+    delete callbacks[message.tag];
+    callback(message);
+});
+
+
