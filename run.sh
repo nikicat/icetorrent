d6a38ffc8b07a87f30fb2d5d972b713a8f0fc2a9
Nikolay Bryskin <nbryskin@yandex-team.ru>
2012-10-19 03:22:44 +0400

    massive refactoring - one step closer to fully working version

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -m
-cmd="cfx run -p profile --package-path=/usr/lib/node_modules/ -b /usr/bin/firefox-nightly $@"
+if [ "$1" = '-d' ]; then
+    binary=/usr/bin/firefox-nightly-debug
+    shift
+else
+    binary=/usr/bin/firefox-nightly
+fi
+cmd="cfx run -p profile --package-path=/usr/lib/node_modules/ -b $binary $@"
 if [ "$1" = '-v' ]; then
     NSPR_LOG_MODULES=all:5 $cmd >output.log 2>&1 &
 else
